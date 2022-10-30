7be1165108e807fe7fb4b9f011bfbce88be3ea63
Jacob Janzen <jacob.a.s.janzen@gmail.com>
2022-10-29 22:57:23 -0500

    deleted test that doesn't work on github

--- pkg/system/user_test.go
+++ pkg/system/user_test.go
@@ -1,7 +1,6 @@
 package system
 
 import (
-	"os"
 	"testing"
 )
 
@@ -14,17 +13,5 @@ func TestGetUsername(t *testing.T) {
 
 	if username != "root" {
 		t.Errorf("expected root got %s", username)
-	}
-}
-
-func TestGetLogin(t *testing.T) {
-	username, err := GetLogin()
-
-	if err != nil {
-		t.Errorf("unexpected error: %v", err)
-	}
-
-	if username != os.Getenv("USER") {
-		t.Errorf("expected %s got %s", os.Getenv("USER"), username)
 	}
 }
